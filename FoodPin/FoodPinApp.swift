e0f51c2fc15efa60edeeac66e72510b4e363c7e6
Daniel <daniel@pink-taco.home>
2021-10-25 22:02:05 +0200

    made swipe on rows in SwiftUI

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Daniel on 2021-10-23.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RestaurantListView()
+        }
+    }
+}
